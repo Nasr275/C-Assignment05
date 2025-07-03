03bfbad10d463c9c5d11037a40acd551789b1703
Abdelrahman Nasr <abdelrhmannasr275@gmail.com>
2025-07-03 23:41:00 +0300

    All except Q1 and Q2

--- C# Assignment05/Program.cs
+++ C# Assignment05/Program.cs
@@ -43,6 +43,43 @@ namespace C__Assignment05
         }
         #endregion
 
+        #region 6.Create a function named MinMaxArray, to return the minimum and maximum values stored in an array, using reference parameters
+
+        void MinMaxArray(int[] arr, ref int minVal, ref int maxVal)
+        {
+            minVal = arr[0];
+            maxVal = arr[0];
+            foreach (int val in arr)
+            {
+                if (val < minVal) minVal = val;
+                if (val > maxVal) maxVal = val;
+            }
+        }
+        #endregion
+
+
+        #region 7.Create an iterative (non-recursive) function to calculate the factorial of the number specified as parameter
+        long Factorial(int n)
+        {
+            long result = 1;
+            for (int i = 2; i <= n; i++)
+                result *= i;
+            return result;
+        }
+        #endregion
+
+        #region 8.Create a function named "ChangeChar" to modify a letter in a certain position (0 based) of a string, replacing it with a different letter
+        string ChangeChar(string str, int pos, char ch)
+        {
+            if (pos < 0 || pos >= str.Length)
+                return str;
+            char[] chars = str.ToCharArray();
+            chars[pos] = ch;
+            return new string(chars);
+        }
+        #endregion
+
+
         static void Main(string[] args)
         {
 
@@ -81,25 +118,40 @@ namespace C__Assignment05
 
             #region Q5 Example
             Console.Write("\nEnter a number to check if it's prime: ");
-            int primeCandidate = int.Parse(Console.ReadLine());
+            int.TryParse(Console.ReadLine(), out int primeCandidate);
             Console.WriteLine(myProgram.IsPrime(primeCandidate) ? "Prime" : "Not Prime");
             #endregion
 
 
-            #region 6.Create a function named MinMaxArray, to return the minimum and maximum values stored in an array, using reference parameters
+            #region Q6 Example
 
+            int[] sampleArray = { 5, 8, 2, 9, 1, 7 };
+            int min = 0, max = 0;
+            myProgram.MinMaxArray(sampleArray, ref min, ref max);
+            Console.WriteLine($"Min: {min}, Max: {max}");
+            #endregion
+
+            #region Q7 Example
+
+            Console.Write("\nEnter number to calculate factorial: ");
+            int.TryParse(Console.ReadLine(), out int factNum);
+            Console.WriteLine($"Factorial of {factNum} is {myProgram.Factorial(factNum)}");
 
             #endregion
 
-            #region 7.Create an iterative (non-recursive) function to calculate the factorial of the number specified as parameter
 
 
-            #endregion
+            #region Q8 Example
 
+            Console.Write("\nEnter a string: ");
+            string original = Console.ReadLine();
+            Console.Write("Enter index to replace: ");
+            int.TryParse(Console.ReadLine(), out int index);
+            Console.Write("Enter new character: ");
+            char.TryParse(Console.ReadLine(), out char newChar);
 
-
-            #region 8.Create a function named "ChangeChar" to modify a letter in a certain position (0 based) of a string, replacing it with a different letter
-
+            string modified = myProgram.ChangeChar(original, index, newChar);
+            Console.WriteLine($"Modified string: {modified}");
 
             #endregion
 
